f8739ff960fec1097f2d2e67d768943d74c2216c
Michel <>
2022-09-19 09:51:43 +0300

    signin and signup update

--- src/components/settings/billing/Billing.jsx
+++ src/components/settings/billing/Billing.jsx
@@ -3,6 +3,9 @@ import { Add, NavigateNext } from '@mui/icons-material';
 
 import './Billing.scss';
 
+import mastercard from '../../../assets/images/mastercard.svg';
+import visa from '../../../assets/images/visa.svg';
+
 import PaperKit from '../../../kits/paper/PaperKit';
 import ButtonKit from '../../../kits/button/ButtonKit';
 
@@ -30,7 +33,7 @@ const Billing = () => {
         <PaperKit className="billing__plan">
           <div className="billing__plan__flex">
             <div>
-              <p>YOUR PLAN</p>
+              <p className="billing__card-title">YOUR PLAN</p>
               <p className="billing__plan__flex__text">Premium</p>
             </div>
             <div className="billing__plan__flex__btn">
@@ -47,10 +50,10 @@ const Billing = () => {
           </div>
         </PaperKit>
         <PaperKit className="billing__payment">
-          <p>PAYMENT METHOD</p>
+          <p className="billing__card-title">PAYMENT METHOD</p>
           <div className="billing__payment__flex">
-            <PaymentMethod type="visa" cvv="1234" />
-            <PaymentMethod type="visa" cvv="1234" />
+            <PaymentMethod type={mastercard} cvv="1234" />
+            <PaymentMethod type={visa} cvv="1234" />
           </div>
           <ButtonKit onClick={addPaymentMethod} startIcon={<Add />}>
             Add New Card
@@ -62,14 +65,14 @@ const Billing = () => {
           />
         </PaperKit>
         <PaperKit className="billing__info">
-          <p>BILLING INFO</p>
+          <p className="billing__card-title">BILLING INFO</p>
           <Info name="Name" address="Address" phone="00000" />
           <Info name="Name" address="Address" phone="00000" />
           <ButtonKit startIcon={<Add />}>Add New Address</ButtonKit>
         </PaperKit>
       </div>
       <div className="billing__invoice">
-        <p style={{ marginBottom: '2rem', fontWeight: 'bold' }}>Invoice History</p>
+        <p style={{ marginBottom: '2rem', fontWeight: '600' }}>Invoice History</p>
         <Invoice date="4 September 2022" amount="$28.9" />
         <Invoice date="4 September 2022" amount="$28.9" />
         <div className="billing__invoice__btn-next">
