ece55ec24715424e25031441c2b4ed6b4c855cfb
Esenushka <prosterling.mien@gmail.com>
2022-10-04 22:41:28 +0600

    feat: add heatmap,2nd select type

---
+++ src/components/marketingSetup/MarketingSetup.jsx
@@ -0,0 +1,212 @@
+import React, { useState } from 'react';
+import CloseIcon from '../../assets/images/ic_close.png';
+import Dates from '../dates/Dates';
+import ButtonKit from '../../kits/button/ButtonKit';
+import TypographyKit from '../../kits/typography/TypographyKit';
+import './MarketingSetup.scss';
+import PaperKit from '../../kits/paper/PaperKit';
+import ContainerKit from '../../kits/container/ContainerKit';
+import BoxKit from '../../kits/box/BoxKit';
+import LinearProgressKit from '../../kits/linearProgress/LinearProgressKit';
+import CompetitionDropdown from '../competitionDropdown/CompetitionDropdown';
+import PlatformIcon from '../../assets/images/ic_select_platform.png';
+import OpacityLogo from '../../assets/images/opacity-logo.png';
+import OrderHeatMap from '../../data/fakeDataMarketing';
+import TextfieldKit from '../../kits/textfield/TextfieldKit';
+import TypeDiscountIcon from '../../assets/images/ic_type-dis.png';
+
+const MarketingSetup = ({ active, setActive }) => {
+  const [progress, setProgress] = useState(33.33);
+  const [platform, setPlatform] = useState('');
+  const [branch, setBranch] = useState('');
+  const [typeDiscount, setTypeDiscount] = useState('');
+  const [links, setLinks] = useState(false);
+  const days = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+  const heatMap = () => {
+    const data = OrderHeatMap.values;
+    const heatMapObj = {
+      Monday: {},
+      Tuesday: {},
+      Wednesday: {},
+      Thusday: {},
+      Friday: {},
+      Saturday: {},
+      Sunday: {},
+    };
+    Object.keys(heatMapObj).forEach((day) => {
+      for (let i = 5; i < 25; i++) {
+        heatMapObj[day] = { ...heatMapObj[day], [i]: {} };
+      }
+    });
+    Object.keys(data).forEach((day) => {
+      Object.keys(heatMapObj[day]).forEach((num) => {
+        heatMapObj[day][num] = data[day][num] || {};
+      });
+    });
+    return Object.values(heatMapObj);
+  };
+  const getProgress = () => {
+    if (progress === 33.33) {
+      return (
+        <div className="left-part-middle">
+          <TypographyKit variant="h4">1.Select platform and branches</TypographyKit>
+          <TypographyKit sx={{ fontSize: '18px' }} color="#637381" variant="subtitle">
+            Proin ut tellus elit nunc, vel, lacinia consectetur condimentum id.
+          </TypographyKit>
+          <CompetitionDropdown
+            rows={['talabat', 'deliveroo']}
+            icon={PlatformIcon}
+            title="Select a Platform"
+            className="top-competition"
+            setRow={setPlatform}
+            select={platform}
+          />
+          <CompetitionDropdown
+            rows={['1 Branch', '2 Branch']}
+            icon={PlatformIcon}
+            title="Select Branches"
+            className="top-competition"
+            setRow={setBranch}
+            select={branch}
+          />
+        </div>
+      );
+    }
+    return (
+      <div className="left-part-middle">
+        <TypographyKit variant="h4">2.Select the Type of the offer</TypographyKit>
+        <TypographyKit sx={{ fontSize: '18px' }} color="#637381" variant="subtitle">
+          Proin ut tellus elit nunc, vel, lacinia consectetur condimentum id.
+        </TypographyKit>
+        <TextfieldKit className="marketing-textfield" label="Menu" variant="outlined" />
+        <TextfieldKit className="marketing-textfield" label="Item" variant="outlined" />
+        <CompetitionDropdown
+          rows={['1 type', '2 type']}
+          icon={TypeDiscountIcon}
+          title="Type of discount"
+          className="top-competition"
+          setRow={setTypeDiscount}
+          select={typeDiscount}
+        />
+      </div>
+    );
+  };
+  return (
+    <div className={`marketing-setup-offer${active ? ' active ' : ''}`}>
+      <PaperKit className="marketing-paper">
+        <ContainerKit>
+          <div className="left-part">
+            <div className="left-part-top">
+              <div>
+                <TypographyKit onClick={() => setProgress(66.66)} variant="h4">
+                  Set up an offer
+                </TypographyKit>
+
+                <img
+                  tabIndex={-1}
+                  role="presentation"
+                  onClick={() => setActive(false)}
+                  src={CloseIcon}
+                  alt="close icon"
+                />
+              </div>
+              <BoxKit className="progress-bar" sx={{ display: 'flex', alignItems: 'center' }}>
+                <BoxKit sx={{ width: '100%', mr: 1 }}>
+                  <LinearProgressKit variant="determinate" value={progress} />
+                </BoxKit>
+                <BoxKit sx={{ minWidth: 35 }}>
+                  <TypographyKit variant="body2" color="text.secondary">{`${Math.round(
+                    progress / 33,
+                  )} / 3`}</TypographyKit>
+                </BoxKit>
+              </BoxKit>
+            </div>
+            {getProgress()}
+            <div className="left-part-bottom">
+              <ButtonKit
+                onClick={() => setProgress(progress - 33.33)}
+                variant="outlined"
+                disabled={!!(progress < 66.66)}>
+                Previous Step
+              </ButtonKit>
+              <ButtonKit
+                onClick={() => setProgress(progress + 33.33)}
+                disabled={!(platform && branch)}
+                variant="contained">
+                Next Step
+              </ButtonKit>
+            </div>
+          </div>
+          <div className="right-part">
+            <div className="right-part-header">
+              <TypographyKit
+                className={`right-part-header_link ${links ? 'active' : ''}`}
+                variant="div">
+                <BoxKit className={links ? 'active' : ''} onClick={() => setLinks(false)}>
+                  <img src={PlatformIcon} alt="platform icon" />
+                  Orders Heat Map
+                </BoxKit>
+                <BoxKit className={links ? 'active' : ''} onClick={() => setLinks(true)}>
+                  <img src={PlatformIcon} alt="platform icon" />
+                  Revenue Heat Map
+                </BoxKit>
+              </TypographyKit>
+              <Dates />
+            </div>
+            <TypographyKit variant="div" className="right-part-main">
+              <TypographyKit className="right-part-main-title" variant="div">
+                <TypographyKit variant="h5">
+                  Max Revenue this week
+                  <TypographyKit variant="span">${OrderHeatMap.max_value}</TypographyKit>
+                </TypographyKit>
+                <TypographyKit variant="h5">
+                  Min Revenue this week
+                  <TypographyKit variant="span">${OrderHeatMap.min_value}</TypographyKit>
+                </TypographyKit>
+              </TypographyKit>
+              <TypographyKit variant="div" sx={{ display: 'flex' }}>
+                <TypographyKit variant="div" className="right-part-main-hour">
+                  <TypographyKit>
+                    <img src={OpacityLogo} alt="Logo" />
+                  </TypographyKit>
+                  {[5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 0].map(
+                    (num) => (
+                      <TypographyKit key={num}>
+                        {num} <span>{num >= 12 ? 'PM' : 'AM'}</span>
+                      </TypographyKit>
+                    ),
+                  )}
+                </TypographyKit>
+                <TypographyKit sx={{ width: '100%' }} variant="div">
+                  <TypographyKit variant="div" className="right-part-main-day">
+                    {days.map((day) => (
+                      <TypographyKit key={day}>{day}</TypographyKit>
+                    ))}
+                  </TypographyKit>
+                  <TypographyKit className="right-part-main-heatmap" variant="div">
+                    {heatMap().map((obj, index) => (
+                      <TypographyKit key={Object.keys(obj)[index]} variant="div">
+                        {Object.keys(obj).map((num, indexObj) => (
+                          <TypographyKit
+                            sx={{
+                              background: `${
+                                obj[indexObj + 5].color ? obj[indexObj + 5].color : '#919EAB1F'
+                              }`,
+                            }}
+                            key={num}
+                          />
+                        ))}
+                      </TypographyKit>
+                    ))}
+                  </TypographyKit>
+                </TypographyKit>
+              </TypographyKit>
+            </TypographyKit>
+          </div>
+        </ContainerKit>
+      </PaperKit>
+    </div>
+  );
+};
+
+export default MarketingSetup;
--- src/pages/marketing/MarketingOffer.jsx
+++ src/pages/marketing/MarketingOffer.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import CloseIcon from '../../assets/images/ic_close.png';
 import Dates from '../../components/dates/Dates';
 import RestaurantDropdown from '../../components/restaurantDropdown/RestaurantDropdown';
 import { restaurantNames } from '../../data/fakeDataDashboard';
@@ -8,19 +7,10 @@ import TypographyKit from '../../kits/typography/TypographyKit';
 import './Marketing.scss';
 import SmartRuleBtnIcon from '../../assets/images/ic_sm-rule.png';
 import SettingFuture from '../../assets/images/ic_setting-future.png';
-import PaperKit from '../../kits/paper/PaperKit';
-import ContainerKit from '../../kits/container/ContainerKit';
-import BoxKit from '../../kits/box/BoxKit';
-import LinearProgressKit from '../../kits/linearProgress/LinearProgressKit';
-import CompetitionDropdown from '../../components/competitionDropdown/CompetitionDropdown';
-import PlatformIcon from '../../assets/images/ic_select_platform.png';
+import MarketingSetup from '../../components/marketingSetup/MarketingSetup';
 
 const MarketingOffer = () => {
   const [active, setActive] = useState(false);
-  const [progress, setProgress] = useState(33.33);
-  const [platform, setPlatform] = useState('');
-  const [links, setLinks] = useState(false);
-
   return (
     <div className="wrapper">
       <div className="top-inputs">
@@ -45,89 +35,7 @@ const MarketingOffer = () => {
           </ButtonKit>
         </div>
       </div>
-      <div className={`marketing-setup-offer${active ? ' active ' : ''}`}>
-        <PaperKit className="marketing-paper">
-          <ContainerKit>
-            <div className="left-part">
-              <div className="left-part-top">
-                <div>
-                  <TypographyKit onClick={() => setProgress(66.66)} variant="h4">
-                    Set up an offer
-                  </TypographyKit>
-                  <img src={CloseIcon} alt="close icon" />
-                </div>
-                <BoxKit className="progress-bar" sx={{ display: 'flex', alignItems: 'center' }}>
-                  <BoxKit sx={{ width: '100%', mr: 1 }}>
-                    <LinearProgressKit variant="determinate" value={progress} />
-                  </BoxKit>
-                  <BoxKit sx={{ minWidth: 35 }}>
-                    <TypographyKit variant="body2" color="text.secondary">{`${Math.round(
-                      progress / 33,
-                    )} / 3`}</TypographyKit>
-                  </BoxKit>
-                </BoxKit>
-              </div>
-              <div className="left-part-middle">
-                <TypographyKit variant="h4">1.Select platform and branches</TypographyKit>
-                <TypographyKit sx={{ fontSize: '18px' }} color="#637381" variant="subtitle">
-                  Proin ut tellus elit nunc, vel, lacinia consectetur condimentum id.
-                </TypographyKit>
-                <CompetitionDropdown
-                  rows={['talabat', 'deliveroo']}
-                  icon={PlatformIcon}
-                  title="Select a Platform"
-                  className="top-competition"
-                  setRow={setPlatform}
-                  select={platform}
-                />
-                <CompetitionDropdown
-                  rows={['talabat', 'deliveroo']}
-                  icon={PlatformIcon}
-                  title="Select a Platform"
-                  className="top-competition"
-                  setRow={setPlatform}
-                  select={platform}
-                />
-              </div>
-              <div className="left-part-bottom">
-                <ButtonKit variant="outlined" disabled>
-                  Previous Step
-                </ButtonKit>
-                <ButtonKit variant="contained">Next Step</ButtonKit>
-              </div>
-            </div>
-            <div className="right-part">
-              <div className="right-part-header">
-                <TypographyKit
-                  className={`right-part-header_link ${links ? 'active' : ''}`}
-                  variant="div">
-                  <BoxKit className={links ? 'active' : ''} onClick={() => setLinks(false)}>
-                    <img src={PlatformIcon} alt="platform icon" />
-                    Orders Heat Map
-                  </BoxKit>
-                  <BoxKit className={links ? 'active' : ''} onClick={() => setLinks(true)}>
-                    <img src={PlatformIcon} alt="platform icon" />
-                    Revenue Heat Map
-                  </BoxKit>
-                </TypographyKit>
-                <Dates />
-              </div>
-              <TypographyKit variant="div" className="right-part-main">
-                <TypographyKit className="right-part-main-title" variant="div">
-                  <TypographyKit variant="h5">
-                    Max Revenue this week
-                    <TypographyKit variant="span">$26,213.89</TypographyKit>
-                  </TypographyKit>
-                  <TypographyKit variant="h5">
-                    Min Revenue this week
-                    <TypographyKit variant="span">$5,213.98</TypographyKit>
-                  </TypographyKit>
-                </TypographyKit>
-              </TypographyKit>
-            </div>
-          </ContainerKit>
-        </PaperKit>
-      </div>
+      <MarketingSetup active={active} setActive={setActive} />
     </div>
   );
 };
