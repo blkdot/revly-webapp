dbe8cc1992dcc86024bc7e4ef14dc9411c7c9598
Esenushka <prosterling.mien@gmail.com>
2022-12-27 22:14:36 +0600

    fix: ads api

--- src/api/planningApi.jsx
+++ src/api/planningApi.jsx
@@ -9,4 +9,4 @@ const { apiUrl } = config;
 export const getOffers = (body) =>
   axios.post(`${apiUrl}/planning/offersv2`, body).catch(handleResponse);
 
-export const getAds = (body) => axios.post(`${apiUrl}/planning/adsv2`, body).catch(handleResponse);
+export const getAds = (body) => axios.post(`${apiUrl}/planning/ads`, body).catch(handleResponse);
