91007dc835786a60839be4fef9c9ce7485ab031e
Esenushka <prosterling.mien@gmail.com>
2022-12-30 13:44:49 +0600

    fix/checked-competitor

--- src/components/competitor/Competitor.jsx
+++ src/components/competitor/Competitor.jsx
@@ -64,9 +64,8 @@ const Competitor = ({ open, opened, platformList }) => {
     };
     // TODO: fix this code
     // eslint-disable-next-line no-console
-    // await sendMail({ ...data, emailTo: 'amine@revly.ae' });
-    // await sendMail({ ...data, emailTo: 'chiekh@revly.ae' });
-    await sendMail({ ...data, emailTo: 'amanbekasylbekov2@gmail.com' });
+    await sendMail({ ...data, emailTo: 'amine@revly.ae' });
+    await sendMail({ ...data, emailTo: 'chiekh@revly.ae' });
     setLoading(true);
     setLoadingReq(false);
     setRestaurant('');
