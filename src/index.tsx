abd14d52acb934f29b7e134615d78ade7a7e071c
Yaroslav Dmytryk <pos1tive@icloud.com>
2023-01-27 15:25:39 +0200

    rename files

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
-import Modal from 'react-modal';
 import ReactDOM from 'react-dom/client';
+import Modal from 'react-modal';
 import { BrowserRouter } from 'react-router-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 Modal.setAppElement('#root');
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById('root')!);
 root.render(
   <BrowserRouter>
     <App />
