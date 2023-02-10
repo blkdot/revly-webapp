fdd41d6c60f0c2f84164602ad7870b56f02d0be9
Tchi <tchi.devica@gmail.com>
2023-02-10 16:08:44 +0300

    change: whilisted account email

--- src/data/whitelisted-email.ts
+++ src/data/whitelisted-email.ts
@@ -1,3 +1,11 @@
-const emailWhitelisted = ['amine@revly.ae'];
+const emailWhitelisted = [
+  'posoh15525@areosur.com',
+  'amine@revly.ae',
+  'chiekh@revly.ae',
+  'amanbek.asylbekov00@gmail.com',
+  'latiks1908@gmail.com',
+  'amanbekasylbekov0@gmail.com',
+  'amanbekasylbekov2@gmail.com',
+];
 
 export default emailWhitelisted;
