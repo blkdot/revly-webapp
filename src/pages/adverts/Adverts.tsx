27672d335c2ecc35d6e2ae497d85fdb5d6b3f2ea
Esenushka <prosterling.mien@gmail.com>
2023-02-13 19:41:45 +0600

    feat: add table

--- src/pages/adverts/Adverts.tsx
+++ src/pages/adverts/Adverts.tsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import Dates from "components/dates/Dates";
 import RestaurantDropdown from "components/restaurantDropdown/RestaurantDropdown";
 import { endOfMonth, format, getYear, subDays } from "date-fns";
-import { PaperKit, TypographyKit } from "kits";
+import { ButtonKit, PaperKit, TypographyKit } from "kits";
 import selectedVendors from 'components/restaurantDropdown/selectedVendors';
 import dayjs from 'dayjs';
 import { enUS } from 'date-fns/locale';
@@ -11,6 +11,8 @@ import { useAtom } from 'jotai';
 import { vendorsAtom } from 'store/vendorsAtom';
 import './Adverts.scss';
 import arrow from 'assets/images/arrow.svg';
+import useTableContentFormatter from 'components/tableRevly/tableContentFormatter/useTableContentFormatter';
+import TableRevly from 'components/tableRevly/TableRevly';
 
 const Adverts = () => {
   const { date } = useDate();
@@ -21,6 +23,7 @@ const Adverts = () => {
     startDate: beforePeriod.startDate,
     endDate: beforePeriod.endDate,
   });
+  const [link, setLink] = useState('list');
   const startDate = new Date(beforePeriodBtn.startDate);
   const endDate = new Date(beforePeriodBtn.endDate);
   const startLocal = startDate.toLocaleDateString();
@@ -51,6 +54,48 @@ const Adverts = () => {
     }
     return selectedVendors('name', display).join(', ');
   }
+  const headersList = [
+    { id: 'brand_name', disablePadding: true, label: 'Brand name' },
+    { id: 'vendor_ids', disablePadding: true, label: 'Branches' },
+    { id: 'start_end_date', disablePadding: true, label: 'Start/Ending Date' },
+    { id: 'start_end_hour', disablePadding: true, label: 'Start/Ending Hour' },
+    { id: 'total_budget', disablePadding: true, label: 'Total Budget' },
+    { id: 'budget_spent', disablePadding: true, label: 'Budget Spent' },
+    { id: 'return_spent', disablePadding: true, label: 'Return on Spent' },
+    { id: 'platform', disablePadding: true, label: 'Platform' },
+    { id: 'status', disablePadding: true, label: 'Status' },
+  ];
+  const {
+    renderCurrency,
+    renderStatus,
+    renderSimpleRowNotCentered,
+    renderScheduleType,
+    renderSimpleRow,
+    renderVendorId,
+    renderTimeSlot,
+  } = useTableContentFormatter();
+  const cellTemplatesObject = {
+    brand_name: renderSimpleRowNotCentered,
+    vendor_ids: renderVendorId,
+    start_end_date: renderSimpleRow,
+    start_end_hour: renderSimpleRow,
+    total_budget: renderCurrency,
+    budget_spent: renderScheduleType,
+    return_spent: renderTimeSlot,
+    platform: renderSimpleRow,
+    status: renderStatus,
+  };
+
+  const renderRowsByHeaderList = (r) =>
+    headersList.reduce(
+      (acc, cur) => ({
+        ...acc,
+        [cur.id]: cellTemplatesObject[cur.id]({ ...r, id: r.offer_id }, cur),
+        id: r,
+        data: r,
+      }),
+      {}
+    );
   return (
     <div className='wrapper'>
       <div className='top-inputs'>
@@ -76,21 +121,30 @@ const Adverts = () => {
         <div className='paper-top-link'>
           <div className='links'>
             <div>
-              <p className='active'>Adverts List</p>
+              <p className={link === 'list' ? 'active' : ''}>Adverts List</p>
               <span>The list of your running adverts for Today</span>
             </div>
             <div>
-              <p>
+              <p className={link === 'performence' ? 'active' : ''}>
                 Adverts Performance
               </p>
               <span>Performance Metrics of you running adverts for Today</span>
             </div>
+            <div className='arrows'>
+              <img tabIndex={-1} role="presentation" onClick={() => setLink('list')} className={link !== 'list' ? 'active' : ''} src={arrow} alt='left-arrow' />
+              <img tabIndex={-1} role="presentation" onClick={() => setLink('performence')} className={link !== 'performence' ? 'active' : ''} src={arrow} alt='right-arrow' />
+            </div>
           </div>
-          <div className='arrows'>
-            <img className='active' src={arrow} alt='left-arrow'/>
+          <ButtonKit>
+            Create new campaign
             <img src={arrow} alt='right-arrow' />
-          </div>
+          </ButtonKit>
         </div>
+        <TableRevly
+          isLoading
+          headers={headersList}
+          rows={[].map(renderRowsByHeaderList)}
+        />
       </PaperKit>
     </div>
   )
