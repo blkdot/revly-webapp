9955fd1735f92187bde9fb7f883b567e88cd8ce6
Tchi <tchi.devica@gmail.com>
2023-02-17 14:25:29 +0300

    fix: work week

--- src/utlls/heatmap/heatmapSelected.tsx
+++ src/utlls/heatmap/heatmapSelected.tsx
@@ -237,7 +237,7 @@ const typeMulti = (
 
     const newData = clearTimeSelected(data);
 
-    if (dayEveryWeekIndex > -1) {
+    if (dayEveryWeekIndex > -1 && daysOrder[dayEveryWeekIndex]) {
       return {
         ...newData,
         [dayEveryWeekIndex]: getHeatmapDataDayNewContent(
