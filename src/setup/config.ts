84956e4337f46a10220d631d78571a6570e403a8
Tchi <tchi.devica@gmail.com>
2023-02-20 23:30:05 +0300

    prepare: receiving .env configuration

--- src/setup/config.ts
+++ src/setup/config.ts
@@ -2,7 +2,7 @@ const config = {
   apiUrl: 'https://revly-backend-prod.herokuapp.com',
   firebaseApiUrl: 'https://us-central1-test-909d1.cloudfunctions.net',
   timeRefreshToken: 900_000, // 15min
-  frontUrl: process.env.REACT_APP_FRONT_ULR || 'https://app.revly.ae/',
+  frontUrl: 'https://app.revly.ae/',
   environment: process.env.REACT_APP_ENV || 'production',
 };
 
