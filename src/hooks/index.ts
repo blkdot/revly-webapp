47974daeb72140d2e71525b56f3e2dcd47b854cf
Yaroslav Dmytryk <pos1tive@icloud.com>
2023-03-04 18:08:34 +0200

    .tsx to .ts

--- src/hooks/index.ts
+++ src/hooks/index.ts
@@ -2,7 +2,6 @@ export { default as useAlert } from './useAlert';
 export { default as useClickAwayListener } from './useClickAwayListener';
 export { default as useCost } from './useCost';
 export { default as useDate } from './useDate';
-export { default as useGlobal } from './useGlobal';
 export { default as useMarketingSetup } from './useMarketingSetup';
 export { default as useQueryState } from './useQueryState';
 export { default as useVendors } from './useVendors';
