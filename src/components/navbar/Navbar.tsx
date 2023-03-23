793718afc327bb29b03c4e1f08e58a9a40dc0028
Esenushka <prosterling.mien@gmail.com>
2023-03-23 22:36:08 +0600

    fix: navbar icon

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -1,20 +1,14 @@
 import { usePlatform } from 'contexts';
 import { logout } from 'firebase-config';
 import { ButtonKit } from 'kits';
-import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { ReactComponent as LogoutIcon } from 'assets/linkIcons/logout.svg';
-import logo from '../../assets/images/logo.png';
-import arrow from '../../assets/images/navbar-arrow.png';
 import smallLogo from '../../assets/images/small-logo.png';
 import { simpleLink } from '../../data/navbarData';
 import Navlink from '../navlink/Navlink';
 import './Navbar.scss';
 
 const Navbar = () => {
-  const [opened, setOpened] = useState(true);
-  const [open, setOpen] = useState(false);
-
   const navigate = useNavigate();
 
   const handleLogout = async () => {
@@ -42,25 +36,14 @@ const Navbar = () => {
     ));
 
   return (
-    <div className={`navbar_wrapper ${opened && 'opened'}`}>
+    <div className='navbar_wrapper'>
       <div
-        onMouseEnter={() => setOpen(true)}
-        onMouseLeave={() => setOpen(false)}
-        className={`Navbar ${(opened || open) && 'opened'}`}
+        className='Navbar'
       >
         <ul>
-          <li className={`Navbar_logo ${(opened || open) && 'opened'}`}>
-            <img className='nav-logo' src={logo} alt='Revly' />
+          <Link to='/dashboard' className='Navbar_logo'>
             <img className='nav-small-logo' src={smallLogo} alt='Revly' />
-            <div
-              role='presentation'
-              tabIndex={-1}
-              onClick={() => setOpened(!opened)}
-              className={`nav-double-arrow ${opened && 'active'}`}
-            >
-              <img src={arrow} alt='Arrow' />
-            </div>
-          </li>
+          </Link>
           {renderSimpleLink(simpleLink)}
         </ul>
         <span className='nav-line' />
